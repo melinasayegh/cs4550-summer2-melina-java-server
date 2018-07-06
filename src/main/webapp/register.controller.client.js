eee65f93b83e0ea14f88b717ba932278e7523669
Melina <sayeghmelina@gmail.com>
2018-07-06 17:16:44 -0400

    modified register_controller IIFE

--- src/main/webapp/register.controller.client.js
+++ src/main/webapp/register.controller.client.js
@@ -1,27 +1,45 @@
 // IIFE: Immediately Invoked Function Expression
 
+(function () {
 
-var registerBtn = jQuery('#registerBtn')
-var usernameFld = $('#username');
-var passwordFld = $('#password');
-var password2Fld = $('#password2');
+    var registerBtn =  $('#registerBtn')
+    var usernameFld =  $('#username');
+    var passwordFld =  $('#password');
+    var password2Fld = $('#password2');
 
-registerBtn.click(registerHandler);
+    registerBtn.click(registerHandler);
 
-function registerHnadler() {
-    var usernameStr = usernameFld.val();
-    var passwordStr = passwordFld.val();
-    var password2Str = password2Fld.val();
+    function registerHandler() {
+        var usernameStr = usernameFld.val();
+        var passwordStr = passwordFld.val();
+        var password2Str = password2Fld.val();
 
+        var userObj = {
+            username: usernameStr,
+            password: passwordStr
+        }
 
-    var userObj = {
-        username: usernameStr,
-        password: passwordStr
+        var userObjStr = JSON.stringify(userObj);
+
+        fetch('/register', {
+            method: 'post',
+            body: userObjStr,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
     }
 
-    console.log(usernameStr);
-    console.log(passwordStr);
-    console.log(password2Str);
+    function confirmPasswords() {
+        // if passwords don't match
+        if (passwordFld !== password2Fld) {
+            // then don't create an account
+            // error message
+            alert('Passwords do not match.')
+        // if passwords do match
+        } else {
+            //continue
+        }
 
-    alert('register button clicked')
-}
+    }
+})()
