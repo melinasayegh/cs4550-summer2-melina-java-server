ef7442027e70ffd64d49ad8df15ff46649300992
Melina <sayeghmelina@gmail.com>
2018-07-09 13:58:31 -0400

    updated registration

--- src/main/java/com/example/webdevsummer2serverjavamelina/services/UserService.java
+++ src/main/java/com/example/webdevsummer2serverjavamelina/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,10 +38,12 @@ public class UserService {
 	
 	/*
 	@GetMapping("/api/user/{userId}")
-	public User findUserById(@RequestBody Integer userId) {
-		return (User) userRepository.findById(userId);
+	public User findUserById(@PathVariable(name="userId") Integer userId) {
+		UserRepository userRepo = UserRepository.getInstance();
+		return userRepo.findUserById(userId);
 	}
-	
+	*/
+	/*
 	@PostMapping("/api/user/{userId}")
 	public updateUser(@RequestBody Integer userId) {
 		return ;
