33f26e3d7b1365a747902753cdb3497a43857de1
Melina <sayeghmelina@gmail.com>
2018-07-09 15:00:56 -0400

    added updateUser() to profile

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -2,9 +2,14 @@
 
     function init() {
 
+        // this should come from session
+        var currentUserId = null;
+        var $username, $firstName, $lastName, $updateBtn;
+
         $username = $("#username");
         $firstName = $("#firstName");
         $lastName = $("#lastName");
+        $updateBtn = $("#updateBtn");
 
         findUserById(7)
             .then(function(user))
@@ -12,21 +17,39 @@
 
     init();
 
-    function findUserById(userId) {
-        return fetch('api/user/' + userId)
-            .then(function (response) {
-                return repsonse.json();
-            })
+    function updateUser() {
+        var user = {
+            firstName: $firstName.val(),
+            lastName: $lastName.val()
+        }
+
+        fetch("/apu/user/" + currentUser.id, {
+            method: 'put',
+            body: JSON.tringify(user),
+            'credentials' : 'include',
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
     }
 
     function renderUser(user) {
         console.log(user);
+
+        currentUser = user;
 
         $username.val(user.username);
         $firstName.val(user.firstName);
         $lastName.val(user.lastName);
     }
 
+
+    function findUserById(userId) {
+        return fetch('api/user/' + userId)
+            .then(function (response) {
+                return repsonse.json();
+            })
+    }
 
     function handleRepsonse() {
 
