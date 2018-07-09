540e6978712e196d71a54268c59550b437fe0582
Melina <sayeghmelina@gmail.com>
2018-07-08 21:15:20 -0400

    added import to UserRepository.java

--- src/main/java/com/example/webdevsummer2serverjavamelina/repositories/UserRepository.java
+++ src/main/java/com/example/webdevsummer2serverjavamelina/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.webdevsummer2serverjavamelina.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
 import com.example.webdevsummer2serverjavamelina.models.User;
 
 
 public interface UserRepository extends CrudRepository<User,Integer> {
-
 }
