cc16feb299deb00f492e03a58735f62ee242d2fe
Melina <sayeghmelina@gmail.com>
2018-07-09 14:54:51 -0400

    add variables to profile controller

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,6 +1,11 @@
 (function () {
 
     function init() {
+
+        $username = $("#username");
+        $firstName = $("#firstName");
+        $lastName = $("#lastName");
+
         findUserById(7)
             .then(function(user))
     }
@@ -12,6 +17,14 @@
             .then(function (response) {
                 return repsonse.json();
             })
+    }
+
+    function renderUser(user) {
+        console.log(user);
+
+        $username.val(user.username);
+        $firstName.val(user.firstName);
+        $lastName.val(user.lastName);
     }
 
 
