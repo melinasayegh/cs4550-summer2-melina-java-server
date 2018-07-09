c47179e4a6fb7c92afb02e4482117ab9e29f3cca
Melina <sayeghmelina@gmail.com>
2018-07-08 23:13:31 -0400

    added user.sql

---
+++ src/main/java/com/example/webdevsummer2serverjavamelina/models/user.sql
@@ -0,0 +1,12 @@
+-- database - user table
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `dob` datetime DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
