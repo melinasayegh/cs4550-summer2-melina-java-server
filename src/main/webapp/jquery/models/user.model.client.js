df8e42b3ae9155c80a0de36628ba89486afa7e02
Melina <sayeghmelina@gmail.com>
2018-07-10 21:06:35 -0400

    added phone, role, and dob to function in user.model.client.js

--- src/main/webapp/jquery/models/user.model.client.js
+++ src/main/webapp/jquery/models/user.model.client.js
@@ -1,5 +1,6 @@
 
-function User(username, password, firstName, lastName, email) {
+function User(username, password, firstName, lastName, email, phone, role, dateOfBirth) {
+    
     this.username = username;
     this.password = password;
     this.firstName = firstName;
